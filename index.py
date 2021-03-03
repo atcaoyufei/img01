9bc3f435fc0bac03b89b780073d34983a56b8f00
caoyufei <atcaoyufei@gmail.com>
2021-03-03 19:49:26 +0800

    0.1

--- index.py
+++ index.py
@@ -1,33 +1,55 @@
 import json
 import logging
+import os
 import time
+from datetime import datetime, timedelta, timezone
 
 from bottle import default_app, template, static_file, request, abort
+from pymongo import MongoClient
 
-from config import drive_data
 from onedrive import OneDrive
 
 DEFAULT_FORMATTER = '%(asctime)s[%(filename)s:%(lineno)d][%(levelname)s]:%(message)s'
 logging.basicConfig(format=DEFAULT_FORMATTER, level=logging.INFO)
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except Exception as e:
+    logging.debug(e)
 
 app = default_app()
+
+mongo_uri = os.environ.get('MONGO_URI')
+client = MongoClient(mongo_uri, connectTimeoutMS=5000, socketTimeoutMS=5000)
+db = client.get_database('db0')
+col = db['oneindex']
+drive_data = col.find_one({'_id': 'img01'})
 
 not_time = int(time.time())
 expires_time = int(drive_data.get('expires_time'))
 one_drive = OneDrive()
 
+
+def get_time():
+    utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
+    return utc_dt.astimezone(timezone(timedelta(hours=8))).strftime('%Y-%m-%d %H:%M:%S')
+
+
 if expires_time <= not_time:
     _data = one_drive.refresh_token(**drive_data)
     access_token = _data.get('access_token')
-    refresh_token = _data.get('refresh_token')
     if not access_token:
         abort(text="refresh token fail.")
 
     drive_data['access_token'] = access_token
-    if refresh_token:
-        drive_data['refresh_token'] = refresh_token
-    with open('config.py', 'w') as f:
-        f.write(f"drive_data = {json.dumps(drive_data, indent=4)}")
+    params = {
+        'access_token': _data.get('access_token'),
+        'refresh_token': _data.get('refresh_token'),
+        'expires_time': int(time.time()) + 3500,
+        'update_date': get_time(),
+    }
+    col.update_one({'_id': drive_data['_id']}, {'$set': params})
 
 one_drive.access_token = drive_data['access_token']
 
